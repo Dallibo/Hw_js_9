eea46ded1b6b5a88217ffdfed7f3cc6244ac982d
Dallibo <denismalyarenko1@gmail.com>
2023-06-13 21:33:15 +0300

    All Hw

--- Hw_js_9.js
+++ Hw_js_9.js
@@ -41,14 +41,122 @@ function getRectanglePerimeter(rect) {
   const perimeter = getRectanglePerimeter(rectangle);
   console.log(`Периметр прямоугольника: ${perimeter}`);
 
+  
 
-function changeRectangleWidth(rect, units) {
-    const newWidth = rect.bottomRight.x - rect.topLeft.x + units;
-    rect.bottomRight.x = rect.topLeft.x + newWidth;
+  function changeRectangleWidth(rect, change) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.bottomRight.x += change;
+    return newRect;
   }
-  console.log("Початковий прямокутник:", rectangle);
-  changeRectangleWidth(rectangle, 2);
-  console.log("Прямокутник зі зміненою шириною:", rectangle);
+  console.log('Початковий прямокутник:');
+  console.log(rectangle);
+  
+  const newWidthRect = changeRectangleWidth(rectangle, 2);
+  console.log('Прямокутник зі зміненою шириною:');
+  console.log(newWidthRect);
+  
+
+
+  function changeRectangleHeight(rect, change) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.bottomRight.y += change;
+    return newRect;
+  }
+  const newHeightRect = changeRectangleHeight(rectangle, 1);
+  console.log('Прямокутник зі зміненою висотою:');
+  console.log(newHeightRect);
+  
+
+
+  function changeRectangleSize(rect, widthChange, heightChange) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.bottomRight.x += widthChange;
+    newRect.bottomRight.y += heightChange;
+    return newRect;
+  }
+  const newSizeRect = changeRectangleSize(rectangle, -1, 2);
+  console.log('Прямокутник зі зміненою шириною та висотою:');
+  console.log(newSizeRect);
+  
+
+
+  function offsetRectangleX(rect, offsetX) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.topLeft.x += offsetX;
+    newRect.bottomRight.x += offsetX;
+    return newRect;
+  }
+  const newXOffsetRect = offsetRectangleX(rectangle, 3);
+  console.log('Зміщений прямокутник по осі X:');
+  console.log(newXOffsetRect);
+  
+
+
+  function offsetRectangleY(rect, offsetY) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.topLeft.y += offsetY;
+    newRect.bottomRight.y += offsetY;
+    return newRect;
+  }
+  const newYOffsetRect = offsetRectangleY(rectangle, 2);
+console.log('Зміщений прямокутник по осі Y:');
+console.log(newYOffsetRect);
+
+
+
+  function offsetRectangleXY(rect, offsetX, offsetY) {
+    const newRect = {
+      topLeft: { ...rect.topLeft },
+      bottomRight: { ...rect.bottomRight }
+    };
+    newRect.topLeft.x += offsetX;
+    newRect.topLeft.y += offsetY;
+    newRect.bottomRight.x += offsetX;
+    newRect.bottomRight.y += offsetY;
+    return newRect;
+  }
+  const newXOffsetYOffsetRect = offsetRectangleXY(rectangle, 3, -1);
+console.log('Зміщений прямокутник по осі X та Y:');
+console.log(newXOffsetYOffsetRect);
+
+
+  
+  function isPointInsideRectangle(rect, point) {
+    const { x, y } = point;
+    const { topLeft, bottomRight } = rect;
+    const { x: leftX, y: topY } = topLeft;
+    const { x: rightX, y: bottomY } = bottomRight;
+  
+    if (x >= leftX && x <= rightX && y >= topY && y <= bottomY) {
+      return true;
+    }
+    return false;
+  }
+
+  const point = { x: 2, y: 1 };
+const isInside = isPointInsideRectangle(rectangle, point);
+console.log(`Точка (${point.x}, ${point.y}) знаходиться всередині прямокутника: ${isInside}`);
+  
+  
+ 
+  
+  
+  
 
 
 
